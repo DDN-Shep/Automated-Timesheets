c0fd9c47f039db174d7c626c6292096fef2c35b5
SheppaA <SheppaA@UKC35N1M1>
2016-04-14 10:38:06 +0100

    Add project files.

---
+++ DDN.Automated.Timesheets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDN.Automated.Timesheets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
